6a73f7fe295a43fe1016b8cbd6c98db22a410381
Matvey <dripkidcandyfloss@mail.ru>
2024-09-10 23:19:50 +0300

    Добавьте файлы проекта.

---
+++ ComplexNumbers/ComplexNumber.cs
@@ -0,0 +1,63 @@
+﻿namespace ComplexNumbers
+{
+    public class ComplexNumber
+    {
+        private double x;
+        private double y;
+
+        public ComplexNumber(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public ComplexNumber Add(ComplexNumber number)
+        {
+            return new ComplexNumber(x + number.getX(), y + number.getY());
+        }
+
+        public ComplexNumber Dif(ComplexNumber number)
+        {
+            return new ComplexNumber(x - number.getX(), y - number.getY());
+        }
+
+        public ComplexNumber Mul(ComplexNumber number)
+        {
+            double newX = x * number.getX() - y * number.getY();
+            double newY = x * number.getY() + y * number.getX();
+            return new ComplexNumber(newX, newY);
+        }
+
+        public ComplexNumber Div(ComplexNumber number)
+        {
+            double newX = (x * number.getX() + y * number.getY()) / (number.getX() * number.getX() + number.getY() * number.getY());
+            double newY = (y * number.getX() - x * number.getY()) / (number.getX() * number.getX() + number.getY() * number.getY());
+            return new ComplexNumber(newX, newY);
+        }
+
+        public string toString()
+        {
+            string stry = "";
+            if (y < 0)
+            {
+                stry = "- " + y.ToString() + "i";
+            }
+            else if (y > 0)
+            {
+                stry = "+ " + y.ToString() + "i";
+            }
+            return x.ToString() + " " + stry;
+        }
+
+
+        public double getX()
+        {
+            return x;
+        }
+        public double getY()
+        {
+            return y;
+        }
+    }
+}
+
