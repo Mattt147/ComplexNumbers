2e0907fd6848672c082cc69def9fa4dd6360f824
dianabaxad <dianka.smirnova.05@bk.ru>
2024-09-11 23:45:47 +0300

    комментарии

--- ComplexNumbers/ComplexNumber.cs
+++ ComplexNumbers/ComplexNumber.cs
@@ -1,26 +1,51 @@
 ﻿namespace ComplexNumbers
 {
+    /// <summary>
+    /// Класс, который представляет комплексное число, состоящее из действительной и мнимой частей.
+    /// Предоставляет методы для выполнения арифметических операций над комплексными числами,
+    /// включая сложение, вычитание, умножение и деление
+    /// </summary>
     public class ComplexNumber
     {
         private double x;
         private double y;
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса
+        /// </summary>
+        /// <param name="x">Действительная часть комплексного числа</param>
+        /// <param name="y">Мнимая часть комплексного числа</param>
         public ComplexNumber(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
 
+        /// <summary>
+        /// Складывает текущее комплексное число с указанным комплексным числом
+        /// </summary>
+        /// <param name="number">Комплексное число, которое нужно сложить с текущим</param>
+        /// <returns>Комплексное число, представляющее результат сложения</returns>
         public ComplexNumber Add(ComplexNumber number)
         {
             return new ComplexNumber(x + number.getX(), y + number.getY());
         }
 
+        /// <summary>
+        /// Вычитает указанное комплексное число из текущего комплексного числа
+        /// </summary>
+        /// <param name="number">Комплексное число, которое нужно вычесть из текущего</param>
+        /// <returns>Комплексное число, представляющее результат вычитания</returns>
         public ComplexNumber Dif(ComplexNumber number)
         {
             return new ComplexNumber(x - number.getX(), y - number.getY());
         }
 
+        /// <summary>
+        /// Умножает текущее комплексное число на указанное комплексное число
+        /// </summary>
+        /// <param name="number">Комплексное число, на которое нужно умножить текущее</param>
+        /// <returns>Комплексное число, представляющее результат умножения</returns>
         public ComplexNumber Mul(ComplexNumber number)
         {
             double newX = x * number.getX() - y * number.getY();
@@ -28,6 +53,11 @@
             return new ComplexNumber(newX, newY);
         }
 
+        /// <summary>
+        /// Делит текущее комплексное число на указанное комплексное число
+        /// </summary>
+        /// <param name="number">Комплексное число, на которое нужно разделить текущее</param>
+        /// <returns>Комплексное число, представляющее результат деления</returns>
         public ComplexNumber Div(ComplexNumber number)
         {
             double newX = (x * number.getX() + y * number.getY()) / (number.getX() * number.getX() + number.getY() * number.getY());
@@ -35,6 +65,12 @@
             return new ComplexNumber(Math.Round(newX,3), Math.Round(newY,3));
         }
 
+
+        /// <summary>
+        /// Переопределяет метод ToString для представления комплексного числа в виде строки
+        /// </summary>
+        /// <returns>Строковое представление комплексного числа в формате "x ± yi", 
+        /// где x - действительная часть, y - мнимая часть.</returns>
         public override string ToString()
         {
             string stry = "";
@@ -53,15 +89,29 @@
             return x.ToString() + " " + stry;
         }
 
-
+        /// <summary>
+        /// Получает значение действительной части комплексного числа
+        /// </summary>
+        /// <returns>Действительная часть комплексного числа</returns>
         public double getX()
         {
             return x;
         }
+
+        /// <summary>
+        /// Получает значение мнимой части комплексного числа
+        /// </summary>
+        /// <returns>Мнимая часть комплексного числа</returns>
         public double getY()
         {
             return y;
         }
+
+        /// <summary>
+        /// Переопределяет метод Equals для сравнения двух комплексных чисел.
+        /// </summary>
+        /// <param name="o">Объект, с которым нужно сравнить текущее комплексное число</param>
+        /// <returns>true, если текущее комплексное число равно указанному объекту, иначе false</returns>
         public override bool Equals(object o)
         {
             ComplexNumber num  = (ComplexNumber)o;
