db32e66e587f10379ac23054096dc42e55279c18
User ulyana <lpkn8103@gmail.com>
2024-09-11 22:13:48 +0300

    интерфейс

--- Interface/Form1.Designer.cs
+++ Interface/Form1.Designer.cs
@@ -28,12 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            textBox5 = new TextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Bisque;
+            label1.Font = new Font("Modern No. 20", 35.9999962F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkGreen;
+            label1.Location = new Point(29, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(724, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Калькулятор комплексных чисел";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Modern No. 20", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(53, 147);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 29);
+            label2.TabIndex = 1;
+            label2.Text = "z1 =";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(130, 147);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(196, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(346, 145);
+            label4.Name = "label4";
+            label4.Size = new Size(28, 31);
+            label4.TabIndex = 4;
+            label4.Text = "+";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(390, 145);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(196, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(592, 145);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 31);
+            label5.TabIndex = 6;
+            label5.Text = "i";
+            label5.Click += label5_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Modern No. 20", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(53, 223);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 29);
+            label3.TabIndex = 7;
+            label3.Text = "z2 =";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(130, 223);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(196, 31);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(390, 221);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(196, 31);
+            textBox4.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(346, 223);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 31);
+            label6.TabIndex = 10;
+            label6.Text = "+";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(592, 221);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 31);
+            label7.TabIndex = 11;
+            label7.Text = "i";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Modern No. 20", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label8.Location = new Point(66, 333);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 29);
+            label8.TabIndex = 12;
+            label8.Text = "z1 ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Modern No. 20", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label9.Location = new Point(196, 333);
+            label9.Name = "label9";
+            label9.Size = new Size(47, 29);
+            label9.TabIndex = 13;
+            label9.Text = "z2 ";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Honeydew;
+            button1.Location = new Point(243, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 33);
+            button1.TabIndex = 14;
+            button1.Text = "=";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "+", "-", "*", "/" });
+            comboBox1.Location = new Point(119, 337);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(60, 23);
+            comboBox1.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Modern No. 20", 21.7499981F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(347, 337);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(421, 31);
+            textBox5.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkSeaGreen;
+            panel1.Location = new Point(2, 104);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(788, 18);
+            panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkSeaGreen;
+            panel2.Location = new Point(2, 284);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(788, 18);
+            panel2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(790, 439);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(textBox5);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private Label label3;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Button button1;
+        private ComboBox comboBox1;
+        private TextBox textBox5;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -6,5 +6,30 @@ namespace Interface
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
