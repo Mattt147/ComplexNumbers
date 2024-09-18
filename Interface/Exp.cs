5f4cce6fda0117a56b70322cb60f836479f13a11
nstsss <nastastrumanowa@gmail.com>
2024-09-19 00:57:46 +0300

    основа заложена, но не доработано

--- Interface/Exp.cs
+++ Interface/Exp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComplexNumbers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,10 +17,73 @@ namespace Interface
         {
             InitializeComponent();
         }
-        
+
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ComplexNumber z1;
+            ComplexNumber z2;
+            string p = "";
+            try
+            {
+                if (textBox1.Text == string.Empty || textBox2.Text == string.Empty || textBox3.Text == string.Empty || textBox4.Text == string.Empty)
+                {
+                    MessageBox.Show("Ошибка: заполнены не все поля");
+                    return;
+                }
+                z1 = new ComplexNumber(double.Parse(textBox1.Text), double.Parse(textBox3.Text), p);
+                z2 = new ComplexNumber(double.Parse(textBox2.Text), double.Parse(textBox4.Text), p);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка: не каждое заполненное поле является числом");
+                return;
+            }
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    textBox5.Text = z1.Add(z2).ToString();
+                    if (textBox5.Text == string.Empty)
+                        textBox5.Text = "0";
+                    break;
+                case 1:
+                    textBox5.Text = z1.Dif(z2).ToString();
+                    if (textBox5.Text == string.Empty)
+                        textBox5.Text = "0";
+                    break;
+                case 2:
+                    textBox5.Text = z1.Mul(z2).ToString();
+                    if (textBox5.Text == string.Empty)
+                        textBox5.Text = "0";
+                    break;
+                case 3:
+                    if (textBox2.Text == "0" && textBox4.Text == "0")
+                    {
+                        textBox5.Text = "";
+                        MessageBox.Show("Ошибка: деление на ноль невозможно");
+                        return;
+                    }
+                    textBox5.Text = z1.Div(z2).ToString();
+                    if (textBox5.Text == string.Empty)
+                        textBox5.Text = "0";
+                    break;
+                default:
+                    MessageBox.Show("Вы не выбрали операцию");
+                    return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
         }
     }
 }
